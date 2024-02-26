4c489574caa70d71b328effabf00b3f1be48f515
tyrie592 <46247477+tyrie592@users.noreply.github.com>
2024-02-25 20:41:54 -0500

    added notes

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 import sys
 import socket
-
+# start text stuff
 if len(sys.argv) != 4:
     print('Usage: python3 client.py <HOSTNAME-OR-IP> <PORT> <FILENAME>')
     sys.exit()
 
+# port and hostname define
 hostnameOrIp = sys.argv[1]
 port = int(sys.argv[2])
 binFilename = sys.argv[3]
 
-
+# socket stuff and connect
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((hostnameOrIp, port))
@@ -17,6 +18,7 @@ except socket.error as e:
     print('ERROR: Connection failed: {404}'.format(e))
     sys.exit()
 
+# data handling
 data = ""
 while "\r\n" not in data:
     data += s.recv(1).decode("utf-8")
@@ -29,12 +31,14 @@ if data == "accio\r\n":
     if data == "accio\r\n":
         s.send("confirm-accio-again\r\n".encode())
 
+# file stuff
 with open(binFilename, 'rb') as file:
     chunk = file.read(10000)
     while chunk:
         s.send(chunk)
         chunk = file.read(10000)
 
+# error handling
 s.settimeout(10)
 try:
     s.recv(1024)
